cdf510c12a71d9f3eb19c4232783a9bee1acbd92
Lino-bird <lino.nikoniko7@gmail.com>
2026-05-11 13:47:24 +0900

    Implement distance and light sensor control for LED

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <SchooMyUtilities.h>
+SchooMyUtilities scmUtils = SchooMyUtilities();
+
+int distance = 0;   // 距離センサー
+int light = 0;      // 明るさセンサー
+int led = 13;       // LED
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+
+  distance = analogRead(A0);
+  light = analogRead(A1);    
+
+  if (distance > 700 && light < 300) {
+
+    digitalWrite(led, HIGH);
+
+  } else {
+
+    digitalWrite(led, LOW); 
+
+  }
+
+}
